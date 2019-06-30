88d65877831d91016cf67c4faab267a01ee5a79d
临时工阿k <122539445@qq.com>
2019-06-30 14:50:48 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+define( "WB_AKEY" , '653894713' );
+define( "WB_SKEY" , '8fc2bbe5adaa3d6166c8c0a700b34895' );
+define( "WB_CALLBACK_URL" , 'https://hao.smallk.net/callback.php' );
---
+++ index.php
@@ -0,0 +1,208 @@
+
+<html >
+<head>
+<title>临时导航站</title>
+<meta charset="UTF-8">
+<meta name="fragment" content="!">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<meta http-equiv="Cache-Control" content="no-transform"/>
+<meta http-equiv="Cache-Control" content="no-siteapp"/>
+<link rel="shortcut icon" href="//upaiyun.smallk.net/usr/uploads/2017/03/685884745.png?.water.90">
+<link rel="apple-touch-icon" href="//upaiyun.smallk.net/usr/uploads/2017/03/685884745.png?.water.90">
+<link  rel="stylesheet" href="css/styles.css">
+<link  rel="stylesheet" href="css/fileinput.min.css">
+<script src="js/jquery.min.js"></script>
+<script>$(document).ready(function(e) {$(".all_below_kuang_a_2").click(function(e) {$(".xuan").toggle();});});</script>
+<script>$(document).ready(function(e) {$(".over").click(function(e) {$(".xuan").toggle();});});</script>
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?a149e177fecdd2a72560b772dd8f78a8";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+</head>
+<div id="all">
+	
+	<div id="all_top">
+		<div id="all_top_sou">
+			<input type="text" id="sou" class="input" placeholder="....">
+			<script>
+			// 关联搜索框的id
+			$('#sou').keydown(function(event){
+			// 判断是不是回车键
+			if(event.keyCode==13){
+				// 对地址进行拼接 在新标签页打开
+				window.open('https://www.baidu.com/s?word='+$(this).val());
+			}
+			})
+			</script>
+<a class="loding" style="background-color: rgb(213, 213, 213);     " target="_blank" href="./Login.php" ondbclick="document.getElementById('sou').value = '';">
+	<img src="/img/toux0.png" style="width: 70px;height: 70px;border-radius: 0px 6px 6px 0px;">
+		</a>
+	</div>
+	</div>
+	<div id="all_below">
+		<div id="all_below_kuang">
+          	
+			<a href="https://sspai.com/" target="_blank" class="all_below_kuang_a"> 
+			<div class="all_below_kuang_img" style="background-color: #ff0000;"> 
+			<img src="https://upaiyun.smallk.net/dh/newimg/shaoshupai.png?.water.90"> 
+			</div> 
+			<div class="all_below_kuang_name"> 
+			<div class="all_below_kuang_nanme2"> 
+			<h3>少数派</h3> 
+			</div> 
+			</div>
+			</a>
+			<a href="https://www.ithome.com/" target="_blank" class="all_below_kuang_a"> 
+			<div class="all_below_kuang_img" style="background-color: #d22222;"> 
+			<img src="https://upaiyun.smallk.net/dh/newimg/ithome.png?.water.90"> 
+			</div> 
+			<div class="all_below_kuang_name"> 
+			<div class="all_below_kuang_nanme2"> 
+			<h3>IT之家</h3> 
+			</div> 
+			</div>
+			</a>
+			<a href="https://www.laomoit.com/" target="_blank" class="all_below_kuang_a"> 
+			<div class="all_below_kuang_img" style="background-color:#c43f44 ;"> 
+			<img src="https://upaiyun.smallk.net/dh/newimg/laoyi.jpg?.water.90"> 
+			</div> 
+			<div class="all_below_kuang_name"> 
+			<div class="all_below_kuang_nanme2"> 
+			<h3>殁漂遥</h3> 
+			</div> 
+			</div>
+			</a>
+			<a href="https://www.coolapk.com/" target="_blank" class="all_below_kuang_a"> 
+			<div class="all_below_kuang_img" style="background-color: #2bbd67;"> 
+			<img src="https://upaiyun.smallk.net/dh/newimg/coolapk.png?.water.90"> 
+			</div> 
+			<div class="all_below_kuang_name"> 
+			<div class="all_below_kuang_nanme2"> 
+			<h3>酷安</h3> 
+			</div> 
+			</div>
+			</a>
+			<a href="http://liulanmi.com/" target="_blank" class="all_below_kuang_a"> 
+			<div class="all_below_kuang_img" style="background-color:#5b723a ;"> 
+			<img src="https://upaiyun.smallk.net/dh/newimg/new_liulanmi.png?.water.90"> 
+			</div> 
+			<div class="all_below_kuang_name"> 
+			<div class="all_below_kuang_nanme2"> 
+			<h3>浏览迷</h3> 
+			</div> 
+			</div>
+			</a>
+			<a href="http://www.weibo.com/" target="_blank" class="all_below_kuang_a"> 
+			<div class="all_below_kuang_img" style="background-color: #ffe99e;"> 
+			<img src="https://upaiyun.smallk.net/dh/newimg/weibo.png?.water.90"> 
+			</div> 
+			<div class="all_below_kuang_name"> 
+			<div class="all_below_kuang_nanme2"> 
+			<h3>微博</h3> 
+			</div> 
+			</div>
+			</a>
+			<a href="https://www.taobao.com/" target="_blank" class="all_below_kuang_a"> 
+			<div class="all_below_kuang_img" style="background-color: #ff5500;"> 
+			<img src="https://upaiyun.smallk.net/dh/newimg/7_fd6028a.png?.water.90"> 
+			</div> 
+			<div class="all_below_kuang_name"> 
+			<div class="all_below_kuang_nanme2"> 
+			<h3>淘宝</h3> 
+			</div> 
+			</div>
+			</a>
+			<a href="https://www.jd.com/" target="_blank" class="all_below_kuang_a"> 
+			<div class="all_below_kuang_img" style="background-color: #f63315;"> 
+			<img src="https://upaiyun.smallk.net/dh/newimg/jd.jpg?.water.90"> 
+			</div> 
+			<div class="all_below_kuang_name"> 
+			<div class="all_below_kuang_nanme2"> 
+			<h3>京东</h3> 
+			</div> 
+			</div>
+			</a>
+			<a href="http://www.iqiyi.com/" target="_blank" class="all_below_kuang_a"> 
+			<div class="all_below_kuang_img" style="background-color: #05ce03;"> 
+			<img src="https://upaiyun.smallk.net/dh/newimg/10_0d65781.png?.water.90"> 
+			</div> 
+			<div class="all_below_kuang_name"> 
+			<div class="all_below_kuang_nanme2"> 
+			<h3>爱奇艺</h3> 
+			</div> 
+			</div>
+			</a>
+			<a href="https://v.qq.com/" target="_blank" class="all_below_kuang_a"> 
+			<div class="all_below_kuang_img" style="background-color: #dcdddc;"> 
+			<img src="https://upaiyun.smallk.net/dh/newimg/tengxship3.png?.water.90"> 
+			</div> 
+			<div class="all_below_kuang_name"> 
+			<div class="all_below_kuang_nanme2"> 
+			<h3>腾讯视频</h3> 
+			</div> 
+			</div>
+			</a>
+			<a href="https://www.bilibili.com/" target="_blank" class="all_below_kuang_a"> 
+			<div class="all_below_kuang_img" style="background-color: #e67093;"> 
+			<img src="https://upaiyun.smallk.net/dh/newimg/new_bilibili.png?.water.90"> 
+			</div> 
+			<div class="all_below_kuang_name"> 
+			<div class="all_below_kuang_nanme2"> 
+			<h3>哔哩哔哩</h3> 
+			</div> 
+			</div>
+			</a>
+			<a href="http://music.163.com/" target="_blank" class="all_below_kuang_a"> 
+			<div class="all_below_kuang_img" style="background-color: #e60026;"> 
+			<img src="https://upaiyun.smallk.net/dh/newimg/new_wangyiyun.png?.water.90"> 
+			</div> 
+			<div class="all_below_kuang_name"> 
+			<div class="all_below_kuang_nanme2"> 
+			<h3>网易云</h3> 
+			</div> 
+			</div>
+			</a>
+			<a href="javascript:void(0);"  class="all_below_kuang_a_2" > 
+			<div class="all_below_kuang_img_2" style="background-color: #eeeeee;"> 
+			<img src="https://upaiyun.smallk.net/dh/newimg/tianjia.png?.water.90"> 
+			</div> 
+			<div class="all_below_kuang_name_2"> 
+			<div class="all_below_kuang_nanme2_2"> 
+			<h3>添加</h3> 
+			</div> 
+			</div>
+			</a>
+		</div>
+	</div>
+	<div class="xuan" style="display: none;">
+		<div class="name">
+		<a>网站名称：</a>
+		<input type="text" name="username" class="name" placeholder="最多5个字">
+		</div>
+		<div class="name_url">
+		<a>网址：</a>
+		<input type="text" name="username" class="name_url" placeholder="请输入网址">
+		</div>
+		<div class="logo">
+		<a>LOGO：</a>
+		<input type="text" name="username" class="logo_color" placeholder="图片网址">
+		<a>OR</a>
+		<input class="upup" id="input-b1" name="input-b1" type="file" class="file">
+		</div>
+		<div class="background">
+		<a>图标背景：</a>
+		<input type="text" name="username" class="background_color" placeholder="带Hex码">
+		<a>OR</a>
+		<input class="upup2" id="input-b1" name="input-b1" type="file" class="file">
+		</div>
+	<button class="save" type="submit" >添加</button>
+	<button class="notsave" type="submit" >清除</button>
+	<button class="over" type="submit" >关闭</button>
+</div>
+</div>
+</html>
